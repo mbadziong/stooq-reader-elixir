49a00a32bdc38167145b470ac854b09314d950a8
Mateusz Badziong <mateuszb@kainos.com>
2017-04-24 14:27:45 +0200

    added mdl layout

--- apps/webapp/web/elm/src/Main.elm
+++ apps/webapp/web/elm/src/Main.elm
@@ -3,7 +3,10 @@ module Main exposing (..)
 import Html exposing (Html, program, div, text)
 import Date exposing (fromTime)
 import Date.Extra exposing (toFormattedString)
+import Material
 import Material.Table as Table
+import Material.Layout as Layout
+import Material.Options as Options
 import Stooq.WebsocketPorts exposing (newMarketIndex, error)
 import Stooq.MarketIndex exposing (MarketIndex)
 import Stooq.Error exposing (Error)
@@ -28,6 +31,7 @@ type alias Model =
     { allIndexes : List MarketIndex
     , currentIndex : Maybe MarketIndex
     , error : Maybe String
+    , mdl : Material.Model
     }
 
 
@@ -37,7 +41,7 @@ type alias Model =
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model [] Nothing Nothing, Cmd.none )
+    ( Model [] Nothing Nothing Material.model, Cmd.none )
 
 
 
@@ -46,6 +50,7 @@ init =
 
 type Msg
     = NewIndex MarketIndex
+    | Mdl (Material.Msg Msg)
     | StooqError Error
 
 
@@ -62,6 +67,9 @@ update msg model =
         StooqError error ->
             { model | currentIndex = Nothing, error = Just error.message } ! []
 
+        Mdl msg_ ->
+            Material.update Mdl msg_ model
+
 
 
 -- VIEW
@@ -69,34 +77,53 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    Table.table []
-        [ Table.thead []
-            [ Table.tr []
-                (List.map
-                    (\index ->
-                        Table.th [] [ text index ]
-                    )
-                    columnNames
-                )
-            ]
-        , Table.tbody []
-            (List.map
-                (\marketIndex ->
-                    Table.tr []
-                        [ Table.th [] [ text (toFormattedString dateFormat (Date.fromTime (toFloat marketIndex.time * 1000))) ]
-                        , Table.th [] [ text marketIndex.wig ]
-                        , Table.th [] [ text marketIndex.wig20 ]
-                        , Table.th [] [ text marketIndex.wig20fut ]
-                        , Table.th [] [ text marketIndex.mwig40 ]
-                        , Table.th [] [ text marketIndex.swig80 ]
-                        ]
-                )
-                (List.take
-                    50
-                    model.allIndexes
-                )
-            )
+    Layout.render Mdl
+        model.mdl
+        [ Layout.fixedHeader
         ]
+        { header =
+            [ Layout.row
+                []
+                [ Layout.title [] [ text "Stooq Reader" ]
+                , Layout.spacer
+                ]
+            ]
+        , drawer = []
+        , tabs = ( [], [] )
+        , main =
+            [ Options.div
+                [ Options.center ]
+                [ Table.table []
+                    [ Table.thead []
+                        [ Table.tr []
+                            (List.map
+                                (\index ->
+                                    Table.th [] [ text index ]
+                                )
+                                columnNames
+                            )
+                        ]
+                    , Table.tbody []
+                        (List.map
+                            (\marketIndex ->
+                                Table.tr []
+                                    [ Table.th [] [ text (toFormattedString dateFormat (Date.fromTime (toFloat marketIndex.time * 1000))) ]
+                                    , Table.th [] [ text marketIndex.wig ]
+                                    , Table.th [] [ text marketIndex.wig20 ]
+                                    , Table.th [] [ text marketIndex.wig20fut ]
+                                    , Table.th [] [ text marketIndex.mwig40 ]
+                                    , Table.th [] [ text marketIndex.swig80 ]
+                                    ]
+                            )
+                            (List.take
+                                50
+                                model.allIndexes
+                            )
+                        )
+                    ]
+                ]
+            ]
+        }
 
 
 
